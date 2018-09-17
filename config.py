1663ead6c52f6d80782b96487fb95c9b8d4d2562
ChristopherSparling <christopher.j.sparling@gmail.com>
2018-09-17 07:20:24 -0400

    Renaming, additional logic

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import random
 """
-audio_dict = {
-    'name' (key): ['path-to-file', (audio-length?), combo-length (numeric for seconds), 'combo text']
+basic_dict = {
+    'name' (key): ['path-to-file', (basic-length?), combo-length (numeric for seconds), 'combo text']
 }
 http://www.fromtexttospeech.com/ use Daisy or George
 """
 basic_dict = {
-    'one':      ['./audio-files/one.mp3',    1, 'Jab'],
-    'two':      ['./audio-files/two.mp3',    1, 'Jab Cross'],
-    'three':    ['./audio-files/three.mp3',  2, 'Jab Cross Hook'],
-    'four':     ['./audio-files/four.mp3',   2.5, 'Jab Cross Hook Cross'],
-    'five':     ['./audio-files/five.mp3',   3, 'Jab Cross Hook Cross Up'],
-    'six':      ['./audio-files/six.mp3',    1, 'Cross'],
-    'seven':    ['./audio-files/seven.mp3',  1, 'Cross Up'],
-    'eight':    ['./audio-files/eight.mp3',  1.5, 'Cross Up Cross'],
-    'nine':     ['./audio-files/nine.mp3',   1.5, 'Hook Cross Hook'],
-    'ten':      ['./audio-files/ten.mp3',    1, 'Cross Hook'],
+    'one':      ['./basic-files/one.mp3',    1, 'Jab'],
+    'two':      ['./basic-files/two.mp3',    1, 'Jab Cross'],
+    'three':    ['./basic-files/three.mp3',  2, 'Jab Cross Hook'],
+    'four':     ['./basic-files/four.mp3',   2.5, 'Jab Cross Hook Cross'],
+    'five':     ['./basic-files/five.mp3',   3, 'Jab Cross Hook Cross Up'],
+    'six':      ['./basic-files/six.mp3',    1, 'Cross'],
+    'seven':    ['./basic-files/seven.mp3',  1, 'Cross Up'],
+    'eight':    ['./basic-files/eight.mp3',  1.5, 'Cross Up Cross'],
+    'nine':     ['./basic-files/nine.mp3',   1.5, 'Hook Cross Hook'],
+    'ten':      ['./basic-files/ten.mp3',    1, 'Cross Hook'],
 }
 # list of tips to be displayed below combo maybe
 tips_list = []
